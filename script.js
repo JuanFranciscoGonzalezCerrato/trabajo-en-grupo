e799cff11e7e3df82ba8d0a625c20cbc74581618
javierprogrameitor <javierprogrameitor@gmail.com>
2023-12-17 08:43:45 +0100

    Inicio de Tarea
    He creado dos cards para empezar a trabajar con ellas en el proyecto

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
-const url = 'https://omgvamp-hearthstone-v1.p.rapidapi.com/info';
+let elementImage = document.getElementById("imgHealth");
+let elementName = document.getElementById("name");
+let button = document.getElementById("changeImage");
+
+let elementImage1 = document.getElementById("imgHealth1");
+let elementName1 = document.getElementById("name1");
+let button1 = document.getElementById("changeImage1")
+
+
+const url = 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cardbacks';
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '8cdf76e32emsh1f9d83de6474589p12529cjsnb0dc6ef04525',
+		'X-RapidAPI-Key': '16dbc09bf0mshbd605ba35088dbcp136201jsn11353c161ca8',
 		'X-RapidAPI-Host': 'omgvamp-hearthstone-v1.p.rapidapi.com'
 	}
 };
 
-
-
-
-async function verify(){
-	try {
-		const response = await fetch(url, options);
-		const result = await response.text();
-		console.log(result);
-	} catch (error) {
-		console.error(error);
-	}
+function getRamdom() {
+    fetch(url, options)
+        .then(response => response.json())
+        .then(data => {
+            let randomIndex = Math.floor(Math.random() * data.length);
+            elementImage.src = data[randomIndex].img;
+            elementName.innerHTML = data[randomIndex].name;
+        })
+        .catch(error => console.error("Error fetching data:", error));
 }
 
-verify();
+function getRamdom1() {
+    fetch(url, options)
+        .then(response => response.json())
+        .then(data => {
+            let randomIndex1 = Math.floor(Math.random() * data.length);
+            elementImage1.src = data[randomIndex1].img;
+            elementName1.innerHTML = data[randomIndex1].name;
+        })
+        .catch(error => console.error("Error fetching data:", error));
+}
+
+
+
+button.addEventListener("click", getRamdom);
+button1.addEventListener("click", getRamdom1);
+
+window.onload = getRamdom;
+window.onload = getRamdom1;
